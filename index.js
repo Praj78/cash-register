968a4bc3a8557207472427c97f5cb34867d565f3
[Prajwal Yeri] <prajwal.yeri@gmail.com>
2021-11-13 15:03:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const billAmount=document.querySelector("#bill-amount");
+const cashGiven=document.querySelector("#cash-given");
+const checkButton=document.querySelector("#check-btn");
+const message=document.querySelector("#error-message");
+const noOfNotes=document.querySelectorAll(".no-of-notes");
+const availableNotes=[2000,500,100,20,10,5,1];
+checkButton.addEventListener("click",function calculateChange(){
+hideMessage();
+if(billAmount.value>0){
+if(cashGiven.value>=billAmount.value){
+const amountToBeReturned=cashGiven.value-billAmount.value;
+changeToBeReturned(amountToBeReturned);
+}else{
+    showMessage("muft me khana chahate ho kya!!")
+}
+}else{
+    showMessage("invalid bill amount")
+}
+});
+function changeToBeReturned(amountToBeReturned){
+    for(let i=0;i<availableNotes.length;i++){
+        const change=Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned%=availableNotes[i]
+        noOfNotes[i].innerText=change;
+    }
+   
+}
+function hideMessage(){
+    message.style.display="none";
+}
+function showMessage(msg){
+    message.style.display="block";
+    message.innerText=msg;
+}
