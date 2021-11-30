28558ca5bf0762041dcea4b15dd52d5cd0d595bd
Praj78 <86727228+Praj78@users.noreply.github.com>
2021-11-30 23:39:49 +0530

    converting strings to number

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const noOfNotes=document.querySelectorAll(".no-of-notes");
 const availableNotes=[2000,500,100,20,10,5,1];
 checkButton.addEventListener("click",function calculateChange(){
 hideMessage();
-if(billAmount.value>0){
-if(cashGiven.value>=billAmount.value){
+if(Number(billAmount.value)>0){
+if(Number(cashGiven.value)>=Number(billAmount.value)){
 const amountToBeReturned=cashGiven.value-billAmount.value;
 changeToBeReturned(amountToBeReturned);
 }else{
