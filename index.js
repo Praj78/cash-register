9dedd8bfb83b2900bbc58d690aa94f7665f6c255
Praj78 <86727228+Praj78@users.noreply.github.com>
2021-11-30 23:24:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ changeToBeReturned(amountToBeReturned);
 });
 function changeToBeReturned(amountToBeReturned){
     for(let i=0;i<availableNotes.length;i++){
-        const change=Math.trunc(amountToBeReturned%availableNotes[i]);
-        amountToBeReturned%=availableNotes[i]
+        const change=Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned=amountToBeReturned%availableNotes[i]
         noOfNotes[i].innerText=change;
     }
    
